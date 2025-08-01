85c8537837a22d82ce76e736c0db3dfa95f46fc7
Anjela Orlanes <anjelaorlanes@Anjelas-MacBook-Pro.local>
2025-08-01 10:08:05 -0700

    Add types where missing on FE; Address console errors-- missing keys, html structure

---
+++ src/app/entities/AdvocateEntity.ts
@@ -0,0 +1,11 @@
+interface AdvocateEntity {
+  id: string;
+  firstName: string;
+  lastName: string;
+  city: string;
+  degree: string;
+  specialties: string[];
+  yearsOfExperience: number;
+  phoneNumber: number;
+  createdAt: Date;
+}
